09568295730727f706460c1a03852a57df9988de
Luiz Guilherme Bandeira <arkanael@gmail.com>
2024-04-12 20:49:15 -0300

    Finalizando a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+
+import styles from './App.module.css'
 import './global.css';
+
+import { Sidebar } from  './components/Sidebar'
 
 export function App() {
 
   return (
    <div>
     <Header/>
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
       <Post 
         author="Luiz Guilherme Bandeira" 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure velit enim exercitationem aperiam voluptatem iusto, ad expedita pariatur blanditiis dolorum odio. Voluptates veniam cum aperiam eaque, quasi molestias sunt." 
@@ -15,6 +22,10 @@ export function App() {
         author="Gabriel" 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure velit enim exercitationem aperiam voluptatem iusto, ad expedita pariatur blanditiis dolorum odio. Voluptates veniam cum aperiam eaque, quasi molestias sunt." 
       />
+      </main>
+    </div>
+
+     
    </div>
   )
 }
